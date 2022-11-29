2213964d7ef7b2b73b4c4abc31b4a09111f343ec
Prashant Kumar <pk2psp@gmail.com>
2022-11-29 13:21:28 +0530

    Socket module added in chat-server to enable real time game

--- chat-server/index.js
+++ chat-server/index.js
@@ -3,15 +3,20 @@ const app = express();
 const server = require('http').createServer(app);
 const welcomHandler = require('./handlers/welcomeHandlers')
 const chatHandler = require('./handlers/chatHandlers')
+const blazingHandler = require('./handlers/blazingHandler')
+const Blaze = require('./Blaze')
 const io = require('socket.io')(server, {
     cors: {
         origin: '*',
       }
 })
 
+const game = new Blaze.BlazingBoard([]);
+
 function onSocketConnection(client) {
     welcomHandler(io, client);
     chatHandler(io, client);
+    blazingHandler(io, client, game)
 }
 
 io.on('connection', onSocketConnection)
